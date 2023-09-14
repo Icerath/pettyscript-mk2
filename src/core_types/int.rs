8279cad2f1c0416e8cb7c4e85695e5f671a7f7fc
Dorje Gilfillan <dorjegilfillan@gmail.com>
2023-09-14 15:47:15 +0200

    Added more binops for int.

--- src/core_types/int.rs
+++ src/core_types/int.rs
@@ -4,14 +4,49 @@ pub type Int = i64;
 
 impl CanObj for Int {
     fn __add__(lhs: &Obj<PtyPtr>, rhs: &Obj<PtyPtr>) -> Obj<PtyPtr> {
-        unsafe {
-            // Safety: at the moment we are assuming the first argument on methods
-            // is guaranteed to be self.
-            let lhs = lhs.downcast_ref_unchecked::<Int>();
-            let rhs = rhs.downcast_ref::<Int>().unwrap();
+        // Safety: at the moment we are assuming the first argument on methods
+        // is guaranteed to be self.
+        let lhs = unsafe { lhs.downcast_ref_unchecked::<Int>() };
+        let rhs = rhs.downcast_ref::<Int>().unwrap();
 
-            Obj::new(lhs.value() + rhs.value()).cast_petty()
-        }
+        Obj::from(lhs.value() + rhs.value())
+    }
+    fn __sub__(lhs: &Obj<PtyPtr>, rhs: &Obj<PtyPtr>) -> Obj<PtyPtr> {
+        // Safety: TODO
+        let lhs = unsafe { lhs.downcast_ref_unchecked::<Int>() };
+        let rhs = rhs.downcast_ref::<Int>().unwrap();
+
+        Obj::from(lhs.value() - rhs.value())
+    }
+    fn __mul__(lhs: &Obj<PtyPtr>, rhs: &Obj<PtyPtr>) -> Obj<PtyPtr> {
+        // Safety: TODO
+        let lhs = unsafe { lhs.downcast_ref_unchecked::<Int>() };
+        let rhs = rhs.downcast_ref::<Int>().unwrap();
+
+        Obj::from(lhs.value() * rhs.value())
+    }
+    fn __div__(lhs: &Obj<PtyPtr>, rhs: &Obj<PtyPtr>) -> Obj<PtyPtr> {
+        // Safety: TODO
+        let lhs = unsafe { lhs.downcast_ref_unchecked::<Int>() };
+        let rhs = rhs.downcast_ref::<Int>().unwrap();
+
+        Obj::from(lhs.value() / rhs.value())
     }
 }
+
 unsafe impl ValueObj for Int {}
+
+#[test]
+fn test_ints() {
+    let value_of = |int: &Obj<PtyPtr>| int.downcast_ref::<Int>().map(Obj::value).copied();
+
+    let mut int = Obj::from(5);
+    int = PtyPtr::__add__(&int, &Obj::from(3));
+    assert_eq!(value_of(&int), Some(8));
+    int = PtyPtr::__sub__(&int, &Obj::from(2));
+    assert_eq!(value_of(&int), Some(6));
+    int = PtyPtr::__mul__(&int, &Obj::from(3));
+    assert_eq!(value_of(&int), Some(18));
+    int = PtyPtr::__div__(&int, &Obj::from(2));
+    assert_eq!(value_of(&int), Some(9));
+}
